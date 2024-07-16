1a0d6ffaee04493b842dedb20d5f5974e86cc3bb
pcthomson <114596617+pcthomson@users.noreply.github.com>
2024-07-17 02:55:04 +1000

    Update fMRItogenes_01_decoding_of_brainmaps.r
    Add clarifying comments

--- gene_decoding_and_gene_enrichment_analysis/01_gene_decoding/fMRItogenes_01_decoding_of_brainmaps.r
+++ gene_decoding_and_gene_enrichment_analysis/01_gene_decoding/fMRItogenes_01_decoding_of_brainmaps.r
@@ -1,8 +1,8 @@
 # -----------------------------------------------------------
 # Project: ASD and ADHD - AT project
-# This code allows you to brain decode genes i.e. to create a list of genes that are spatially 
-# correlated with the imaging maps (e.g. between group t-contrast rfMRI maps) after rigorous
-# FDR correction. The statistical modelling simply test the beta values of the gene-imaging 
+# This code allows you to do gene decoding of brainmaps, i.e. to create a list of genes that are spatially 
+# correlated with the neuroimaging maps (e.g., group t-contrast resting state functional MRI maps) after rigorous
+# FDR correction. The statistical modelling simply tests the beta values of the gene-imaging 
 # correlations of each donor of the Allen Brain Atlas against 0.
 # Prior running this analysis, make sure to upload your MRI brain map in NeuroVault 
 # (https://neurovault.org/my_collections/?q=). The variable "image" is the NeuroVault ID value.
@@ -12,21 +12,22 @@
 
 
 
-# By editing gene_decode.r you can carry out gene decoding either for the whole brain and the cortex.
+# By editing fMRItogenes_function_gene_decode.r you can carry out gene decoding either for the whole brain or the cortex. 
+# Currently, it is set to carry out decoding for the whole brain. 
 
-# edit this, gene_decode.r
+# edit this to give the path to the fMRItogenes_function_gene_decode.r file
 source("fMRItogenes_function_gene_decode.r") 
 
-# edit this, your study root folder
+# edit this to your study root folder
 dataDir <- getwd()
 
-# edit this, output folder
+# edit this to the name of your desired output folder
 measure <- "results_of_gene_decoding"
 
-# edit this, the image identifier on NeuroVault (id)
+# edit this to the image identifier on NeuroVault (id)
 image <- "790618" 
 
-# this is the actual gene decoding calculation
+# this line runs the actual gene decoding calculation
 res = gene_decode(dataDir, image, measure, dbs, term, maxterms, prefix, dbs)
 
 
